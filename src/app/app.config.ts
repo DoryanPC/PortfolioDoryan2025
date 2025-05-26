e031c9daf27d89674a79b686d59f338961c94952
Doryan Pérez <doryan.perez@bcr.gob.sv>
2025-05-26 14:02:34 -0600

    cambios antes de 18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(BrowserAnimationsModule),
     provideHttpClient(),
-  ],
+    provideStore()
+],
 };
